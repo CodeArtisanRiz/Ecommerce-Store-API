f50b40194549d0c99e45a0a0b9e5c6012885119e
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-07-04 01:31:29 +0530

    typo fix

--- ecommerce_store/products/urls.py
+++ ecommerce_store/products/urls.py
@@ -4,7 +4,7 @@ from . import views
 # localhost:8000/chai
 # localhost:8000/chai/order
 urlpatterns = [
-    path('', views.products, name='all_tea'),
+    path('', views.products, name='products'),
     # path('order/', views.order, name='order'),
 
 ]
