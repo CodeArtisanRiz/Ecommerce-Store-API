fdd2816f52ea9b6efa5feb55fd64651c4dd5eeb2
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-07-04 13:05:44 +0530

    Added Product model, configured media directory, and updated URLs
    - Created Product model in the products app. - Configured media directory for handling media files. - Updated URLs to serve media files during development. - Made and applied migrations for the new Product model. - Registered Product model in admin.py for admin interface.

--- ecommerce_store/products/models.py
+++ ecommerce_store/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Products(models.Model):
+    STATUS_CHOICES = (
+        ('A', 'Active'),
+        ('D', 'Deactivated'),
+    )
+    id = models.AutoField(primary_key=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='products_images/')
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
