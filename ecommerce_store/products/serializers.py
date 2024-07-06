109eb9bcdd641d4067d0b31ea6a9f3caa44db365
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-07-06 16:24:10 +0530

    Add field category to serializers

--- ecommerce_store/products/serializers.py
+++ ecommerce_store/products/serializers.py
@@ -4,4 +4,4 @@ from .models import Products
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
-        fields = ['id', 'status', 'name', 'description', 'price', 'image', 'created_at', 'updated_at']
+        fields = ['id', 'status', 'name', 'category', 'description', 'price', 'image', 'created_at', 'updated_at']
